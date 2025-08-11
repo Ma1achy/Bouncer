d7d9b5bf5a78f8a181aa899eb91bb7b07318200a
Malachy <149696299+Ma1achy@users.noreply.github.com>
2025-08-11 18:59:54 +0100

    Delete build/lib/limen directory

--- build/lib/limen/decorators/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Decorators module exports
-"""
-from .base import AccessControlDecorator
-from .access_decorators import private, protected, public
-from .friend_decorator import friend
-
-__all__ = ['AccessControlDecorator', 'private', 'protected', 'public', 'friend']
